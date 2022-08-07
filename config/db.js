75a51499f2793ea139ec52c6366f5fbac8d102fd
ANS <36783010+answebdev@users.noreply.github.com>
2022-08-06 20:17:32 -0700

    connect to MongoDB

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  });
+
+  console.log('MongoDB Connected');
+};
+
+module.exports = connectDB;
+
+// Need to create a MongoDB project, cluster, etc.
+// and use the connection string in 'config.env' instead of the current 'localhost' string.
+
+// See around 36:50
+
+// STOPPED at 39:40
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // To run server in terminal (in root):
 // 'npm run server' (for nodemon), OR 'node server.js'
+
+// IMPORTANT: This 'dotenv' line should be at the very top of the file here,
+// because everything else down below in this file has access to this 'process.env' syntax,
+// so this line needs to go at the very top:
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
 
 const app = express();
 
@@ -16,4 +24,16 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+// Handle server error
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+
+  // Stop server nicely without any crashes -
+  // this will just show us the error being logged
+  // instead of the complete crash stack (that really long stack of messages); easier to read:
+  server.close(() => process.exit(1));
+});
