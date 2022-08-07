a1a04958388b11932e16ee512c0b2c14d5604a78
ANS <36783010+answebdev@users.noreply.github.com>
2022-08-06 20:21:29 -0700

    connect to MongoDB

--- config/db.js
+++ config/db.js
@@ -5,8 +5,10 @@ const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
+
+    // The following are no longer supported:
+    // useCreateIndex: true,
+    // useFindAndModify: true,
   });
 
   console.log('MongoDB Connected');
